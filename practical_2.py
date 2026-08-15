2d1bad7c222655a1c8766aa5eb79b7dce31077f9
Krupa  J. Baldha <krupabld12@gmail.com>
2026-08-15 22:44:37 +0530

    Add Files Via Uploaded

--- practical_2.py
+++ practical_2.py
@@ -1,4 +1,5 @@
 #Demonstrate creation and manipulation of list, tuple, set, dictionary and string with their built-in methods.
+print("------ List ------")
 l=[5,4,3,2]
 print(l)
 print(type(l))
@@ -27,23 +28,25 @@ l.extend(s)
 print("after extending set:",l)
 
 #output:
-'''[5, 4, 3, 2]
-<class 'list'>
-length 4
-after append [5, 4, 3, 2, 1]
-after insert: [5, 6, 4, 3, 2, 1]
-after remove: [5, 4, 3, 2, 1]
-count of 3: 1
-after sorting: [1, 2, 3, 4, 5]
-cleared list: []
-after pop up: [1, 2, 3, 5]
-after reverse: [1, 2, 3, 5]
-length: 4
-slice [2, 3]
-odd no. present in the list: [1, 3]
-last two second numbers: [2, 3]
-after extending set: [1, 2, 3, 5, 200, 100, 300]'''
+'''------ List ------
+   [5, 4, 3, 2]
+   <class 'list'>
+   length 4
+   after append [5, 4, 3, 2, 1]
+   after insert: [5, 6, 4, 3, 2, 1]
+   after remove: [5, 4, 3, 2, 1]
+   count of 3: 1
+   after sorting: [1, 2, 3, 4, 5]
+   cleared list: []
+   after pop up: [1, 2, 3, 5]
+   after reverse: [1, 2, 3, 5]
+   length: 4
+   slice [2, 3]
+   odd no. present in the list: [1, 3]
+   last two second numbers: [2, 3]
+   after extending set: [1, 2, 3, 5, 200, 100, 300]'''
 
+print("------ Tuple ------")
 t=(10,15,20,25,30)
 print(type(t))
 print("count of 50:",t.count(50))
@@ -54,14 +57,16 @@ print("odd no. present in the list:",l[::2])
 print("last two second numbers",t[-3:-1])
 
 #output:
-'''<class 'tuple'>
-count of 50: 0
-index of 30: 4
-length 5
-slice (15, 20)
-odd no. present in the list: [1, 3, 5]
-last two second numbers (20, 25)'''
+'''------ Tuple ------
+   <class 'tuple'>
+   count of 50: 0
+   index of 30: 4
+   length 5
+   slice (15, 20)
+   odd no. present in the list: [1, 3, 5]
+   last two second numbers (20, 25)'''
 
+print("------ Set ------")
 s={"a,b,c,d"}
 print(type(s))
 s.add("e")
@@ -70,10 +75,12 @@ s.remove("e")
 print("after the removine one ele. of set:",s)
 
 #output:
-'''<class 'set'>
-after the addition of set: {'e', 'a,b,c,d'}
-after the removine one ele. of set: {'a,b,c,d'}'''
+'''------ Set ------
+   <class 'set'>
+   after the addition of set: {'e', 'a,b,c,d'}
+   after the removine one ele. of set: {'a,b,c,d'}'''
 
+print("------ Dictionary ------")
 d={'a':1,'b':2,'c':3,'d':4,'e':5}
 print(type(d))
 print(d)
@@ -92,13 +99,14 @@ d1.pop('w')
 print("after pop",d1)
 
 #output:
-'''<class 'dict'>
-{'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
-dict_keys(['a', 'b', 'c', 'd', 'e'])
-dict_values([1, 2, 3, 4, 5])
-dict_items([('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5)])
-copy dictionary of d: {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
-updated dictionary {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'x': 6, 'y': 7, 'z': 8}
-2
-after set {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'x': 6, 'y': 7, 'z': 8, 'w': None}
-after pop {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'x': 6, 'y': 7, 'z': 8}'''
+'''------ Dictionary ------
+   <class 'dict'>
+   {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+   dict_keys(['a', 'b', 'c', 'd', 'e'])
+   dict_values([1, 2, 3, 4, 5])
+   dict_items([('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5)])
+   copy dictionary of d: {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5}
+   updated dictionary {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'x': 6, 'y': 7, 'z': 8}
+   2
+   after set {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'x': 6, 'y': 7, 'z': 8, 'w': None}
+   after pop {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'x': 6, 'y': 7, 'z': 8}'''
